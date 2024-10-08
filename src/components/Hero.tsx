33940797753403467b3a7609df1ce4e9650995b6
Fabián Espitia <fabianespitia370@gmail.com>
2024-10-08 11:47:56 -0500

    fade effect added again

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,5 @@
+import { Fade } from "react-awesome-reveal";
+
 export default function Hero() {
   const links = [
     { label: "Actualiza Hogar", href: "/update_home", id: 1 },
@@ -14,19 +16,20 @@ export default function Hero() {
   return (
     <section className="relative flex items-center pt-16 pb-20">
       <div className="mx-auto text-center">
-        <h1 className="text-6xl md:text-7xl md:mb-4 font-extrabold text-secondary_blue">
-          Spotinet
-        </h1>
+        <Fade triggerOnce>
+          <h1 className="text-6xl md:text-7xl md:mb-4 font-extrabold text-secondary_blue">
+            Spotinet
+          </h1>
 
-        <p className="text-xl text-white mt-10">
-          Por favor selecciona el servicio que deseas utilizar
-        </p>
-        <section className="flex flex-col items-center md:flex-row md:space-x-12 space-y-11 md:space-y-0 mt-14">
-          {links.map((link, index) => (
-            <a
-              key={link.id}
-              href={link.href}
-              className={`rounded-lg text-white w-48 h-48 flex items-center justify-center shadow-md hover:shadow-lg
+          <p className="text-xl text-white mt-10">
+            Por favor selecciona el servicio que deseas utilizar
+          </p>
+          <section className="flex flex-col items-center md:flex-row md:space-x-12 space-y-11 md:space-y-0 mt-14">
+            {links.map((link, index) => (
+              <a
+                key={link.id}
+                href={link.href}
+                className={`rounded-lg text-white w-48 h-48 flex items-center justify-center shadow-md hover:shadow-lg
                 ${
                   index < 3
                     ? "bg-netflix bg-cover bg-center bg-no-repeat"
@@ -34,14 +37,15 @@ export default function Hero() {
                 }
                 border-2 border-gray-400 hover:border-secondary_blue duration-200 transition-all
               `}
-              aria-label={link.label}
-            >
-              <span className="bg-black bg-opacity-50 px-4 py-2 rounded text-lg font-semibold tracking-wide">
-                {link.label}
-              </span>
-            </a>
-          ))}
-        </section>
+                aria-label={link.label}
+              >
+                <span className="bg-black bg-opacity-50 px-4 py-2 rounded text-lg font-semibold tracking-wide">
+                  {link.label}
+                </span>
+              </a>
+            ))}
+          </section>
+        </Fade>
       </div>
     </section>
   );
