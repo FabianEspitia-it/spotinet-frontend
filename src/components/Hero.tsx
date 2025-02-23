d634f1a6feca95a1d8d9f4b38804bfce173c1bca
FabianEspitia-it <fabianespitia370@gmail.com>
2025-02-23 15:08:20 -0500

    add comment to deploy

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,6 +13,8 @@ export default function Hero() {
     { label: "Código de inicio de sesión", href: "/session_code", id: 3 },
   ];
 
+  // add comment to deploy
+
   return (
     <section className="relative flex items-center pt-10 pb-20">
       <div className="mx-auto text-center pt-10">
