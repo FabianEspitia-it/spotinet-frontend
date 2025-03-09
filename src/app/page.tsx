41b0befb5cab21946206d727f7ac5d10cbc05086
FabianEspitia-it <fabianespitia370@gmail.com>
2025-03-09 16:41:04 -0500

    add fade to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,25 @@
 
 import Hero from "@/components/Hero";
 import Image from "next/image";
+import { Fade } from "react-awesome-reveal";
 
 export default function Home() {
   return (
     <div className="relative min-h-screen">
-      <Image
-        src="/images/fondo_spotinet-min.webp"
-        alt="Background"
-        layout="fill"
-        objectFit="cover"
-        quality={100}
-        className="z-0"
-      />
+      <Fade triggerOnce>
+        <Image
+          src="/images/fondo_spotinet-min.webp"
+          alt="Background"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          className="z-0"
+        />
 
-      <main className="relative z-10 flex flex-col justify-center items-center flex-1 px-8 max-w-screen-xl mx-auto">
-        <Hero />
-      </main>
+        <main className="relative z-10 flex flex-col justify-center items-center flex-1 px-8 max-w-screen-xl mx-auto">
+          <Hero />
+        </main>
+      </Fade>
     </div>
   );
 }
