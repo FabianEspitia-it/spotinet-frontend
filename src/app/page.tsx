25c8a5ab6fcb13d0cc590fbd8fc58e3ac1aa003b
FabianEspitia-it <fabianespitia370@gmail.com>
2025-03-09 16:09:05 -0500

    fix bg img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import Hero from "@/components/Hero";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-hero-pattern bg-cover bg-center">
-      <main className="flex flex-col justify-center items-center flex-1 px-8 max-w-screen-xl mx-auto">
+    <div className="relative min-h-screen">
+      <Image
+        src="/images/fondo_spotinet-min.webp"
+        alt="Background"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        className="z-0"
+      />
+
+      <main className="relative z-10 flex flex-col justify-center items-center flex-1 px-8 max-w-screen-xl mx-auto">
         <Hero />
       </main>
     </div>
