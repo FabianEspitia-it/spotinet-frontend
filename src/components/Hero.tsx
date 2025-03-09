510637ccfe41a87602aceab6c3cd968ab4f369da
FabianEspitia-it <fabianespitia370@gmail.com>
2025-03-09 13:57:26 -0500

    fix main sections

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,63 +3,84 @@ import Image from "next/image";
 
 export default function Hero() {
   const links = [
-    { label: "Actualizar Hogar", href: "/update_home", id: 1 },
-    { label: "Estoy de viaje", href: "/temporal_access", id: 2 },
+    { label: "Actualizar Hogar", href: "/update_home", img: "/images/Net.png" },
+    {
+      label: "Estoy de viaje",
+      href: "/temporal_access",
+      img: "/images/Net.png",
+    },
     {
       label: "Código de inicio de sesión",
       href: "/session_netflix_code",
-      id: 5,
+      img: "/images/Net.png",
     },
     {
       label: "Restablecimiento de contraseña",
       href: "/password_reset",
-      id: 4,
+      img: "/images/Net.png",
     },
-    { label: "Código de inicio de sesión", href: "/session_code", id: 3 },
+    {
+      label: "Código de inicio de sesión",
+      href: "/session_code",
+      img: "/images/dis.png",
+    },
   ];
 
-  // add comment to deploy
-
   return (
-    <section className="relative flex items-center pt-10 pb-20">
-      <div className="mx-auto text-center pt-10">
-        <Fade triggerOnce>
-          <Image
-            src="/images/final_logo.png"
-            alt="Spotinet Logo"
-            width={330}
-            height={330}
-            className="mx-auto"
-          />
+    <>
+      <section className="relative flex items-center pt-10 pb-20">
+        <div className="mx-auto text-center pt-10">
+          <Fade triggerOnce>
+            <Image
+              src="/images/final_logo.png"
+              alt="Spotinet Logo"
+              width={330}
+              height={330}
+              className="mx-auto"
+            />
 
-          <p className="text-lg text-white mt-10">
-            Por favor selecciona el servicio que deseas utilizar:
-          </p>
-          <section className="flex flex-col items-center md:flex-row md:space-x-0 space-y-0 md:space-y-0 mt-3">
-            {links.map((link, index) => (
-              <a
-                key={link.id}
-                href={link.href}
-                className={`text-white w-44 h-44 flex items-center justify-center shadow-md hover:shadow-lg
+            <p className="text-lg text-white mt-10">
+              Por favor selecciona el servicio que deseas utilizar:
+            </p>
+
+            <section className="flex flex-col items-center md:flex-row mt-3">
+              {links.map((link, index) => (
+                <a
+                  key={index}
+                  href={link.href}
+                  className={`text-secondary_blue w-44 h-44 border-2 border-secondary_blue flex flex-col items-center justify-center 
+      bg-principal_blue shadow-md hover:bg-secondary_blue hover:text-principal_blue duration-200 transition-all 
       ${
-        index <= 3
-          ? "bg-netflix bg-cover bg-center bg-no-repeat"
-          : "bg-disney bg-cover bg-center bg-no-repeat"
-      }
-      border-2 border-secondary_blue duration-200 transition-all
-      ${index === 0 ? "rounded-tl-2xl rounded-bl-2xl" : ""} 
-      ${index === links.length - 1 ? "rounded-br-2xl rounded-tr-2xl" : ""}
-    `}
-                aria-label={link.label}
-              >
-                <span className="bg-black bg-opacity-50 px-2 py-1 rounded text-md font-semibold tracking-wide text-center flex flex-wrap justify-center">
-                  {link.label}
-                </span>
-              </a>
-            ))}
-          </section>
-        </Fade>
-      </div>
-    </section>
+        index === 0
+          ? "rounded-l-xl"
+          : index === links.length - 1
+          ? "rounded-r-xl"
+          : ""
+      }`}
+                  aria-label={link.label}
+                >
+                  <Image
+                    src={link.img}
+                    alt={link.label}
+                    width={link.img === "/images/Net.png" ? 34 : 90}
+                    height={10}
+                  />
+                  <span className="mt-2 text-md font-semibold text-center">
+                    {link.label}
+                  </span>
+                </a>
+              ))}
+            </section>
+          </Fade>
+        </div>
+      </section>
+      <Image
+        src="/images/Ayuda.png"
+        alt="Pregunta"
+        width={45}
+        height={45}
+        className="md:absolute md:bottom-6 md:right-6 md:mb-0 md:cursor-pointer mb-8"
+      />
+    </>
   );
 }
